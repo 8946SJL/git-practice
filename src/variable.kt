7ff14f1de4dc544bb18ac4a1888068603746fc81
8946SJL <eutume5@gmail.com>
2023-10-25 18:34:40 +0900

    변수 이름 짓기

---
+++ src/variable.kt
@@ -0,0 +1,119 @@
+package src
+
+
+/*규칙
+변수는 명사로 작명합니다.
+예를 들어, 이름은 name, 나이는 age, 제목은 title과 같이 짓습니다.
+
+변수의 시작은 항상 소문자입니다.
+
+여러 단어를 조합해서 작명해야 하는 경우 단어를 연결할 때마다 첫 글자를 대문자로 합니다.
+예를 들어, 이름을 성(last name)과 이름(first name)으로 나누어 다룬다면 성은 lastName으로 이름은 firstName으로 짓습니다.
+단어를 연결할 때 연결할 단어의 첫 글자를 대문자로 표기하는 방식을 카멜 케이스(Camel case)라고 합니다.*/
+
+
+fun main() {
+
+    //1. 유저이름
+    val username
+    //2. 유저 이름 입력 폼
+    val usernameForm
+    //3. 비밀번호
+    val password
+    //4. 재확인 비밀번호
+    val repeatPassword
+    //5. 검증된 비밀번호
+    val certifiedPassword
+    //6. 경고 메시지
+    val warningMessage
+    //7. 폼 제출 버튼
+    val formSubmitButton
+    //8. 취소 버튼
+    val cancelButton
+    //9. 배경 색상
+    val backgroundColor
+    //10. 텍스트 색상
+    val textColor
+    //11. 다른 뷰를 감싸고 있는 뷰
+    val coverView
+    //12. 홀수인지 판명
+    val oddNumber
+    //13. 짝수인지 판명
+    val evenNumber
+    //14. 자식 프로퍼티를 소유하고 있는지 판명
+    val proveHaveProperty
+    //15. 빈 값인지 판명
+    val nullNumber
+    //16. 숨김 처리가 되어있는지 판명
+    val hideNumber
+    //17. 로딩 중인지 판명
+    val loadingNow
+    //18. 인터넷이 켜진 상태인지 판명
+    val turnOnInternet
+//19. 디바이스가 태블릿 인지 판명
+    val proveDeviceTablet
+//20. 사용 불가능한 상태인지 판명
+    val useless
+//21. 숨김처리된 상태인지 판명
+    val hide
+    //22. 다크모드인지 판명
+    val darkMode
+//23. 이미지 주소
+    val imageUrl
+//24. 생성일
+    val date
+    //25. 수정일
+    val modifyDay
+    //26. 태그 목록
+    val tagList
+//27. 검색결과
+    val searchResult
+    //28. 필터링 한 결과
+    val filterResult
+    //29. 가격
+    val price
+    //30. 할인율
+    val discountRate
+    //31. 할인된 가격
+    val discountPrice
+    //32. 최신 포스트 목록
+    val latestPost
+    //33. 인기 포스트 목록
+    val popPost
+    //34. 구매한 상품 목록
+    val buyProduct
+    //35. 가장 많이 팔리는 상품들
+    val popProduct
+    //36. 새로고침 버튼
+    val refreshButton
+    //37. 로딩 버튼
+    val loadingButton
+    //38. 저장 버튼
+    val saveButton
+    //39. 회원가입 버튼
+    val joinButton
+    //40. 로그인 버튼
+    val loginButton
+    //41. URL 복사 버튼
+    val copyButton
+    //42. 전체화면 버튼
+    val fullScreen
+    //43. 수정된 댓글
+    val modifiedReview
+    //44. 삭제된 댓글
+    val deletedReview
+    //45. 임시저장된 포스트
+    val lsavePost
+
+    //46. 즐겨찾기한 포스트 목록
+    val enjoyPostList
+    //47. 친구 목록
+    val friendList
+    //48. 정렬된 아이템 목록
+    val arrayItemList
+    //49. 시작일
+    val startDate
+    //50. 종료일
+    val endDate
+
+}
